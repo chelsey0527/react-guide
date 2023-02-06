09f1105d4d2752b71c0e0d955fefa3fe0e27f09f
chelsey0527 <chelsey0527@gmail.com>
2023-02-06 15:59:12 +0800

    add functions

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   // method 1: three states // most prefered
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setenteredAmount] = useState('');
@@ -47,9 +47,8 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date (enteredDate),
     };
-
-    console.log(expenseData);
     
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setenteredAmount('');
     setenteredDate('');
