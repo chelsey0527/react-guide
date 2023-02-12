68a7685d7a16bc5f7912f64d0b6a1d6c7ad539d1
chelsey0527 <chelsey0527@gmail.com>
2023-02-12 11:41:36 +0800

    finish assignment 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpenses/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {title: 'toilet paper', amount: 94.12, date: new Date(2021, 3, 6)},
+  {title: 'car Insurance', amount: 294.67, date: new Date(2021, 4, 29)},
+  {title: 'banana', amount: 100.23, date: new Date(2021, 2, 28)},
+];
+
 function App() {
-  const expenses = [
-    {title: 'toilet paper', amount: 94.12, date: new Date(2021, 3, 6)},
-    {title: 'car Insurance', amount: 294.67, date: new Date(2021, 4, 29)},
-    {title: 'banana', amount: 100.23, date: new Date(2021, 2, 28)},
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    // add new array & pull out the existent array
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
